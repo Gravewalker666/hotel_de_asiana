490190404e4579a69699724a8fc0c3b653bea244
Gravewalker666 <heshanandrews99@gmail.com>
2020-10-12 11:07:12 +0530

    Update test data script

--- scripts/test_data.sql
+++ scripts/test_data.sql
@@ -1,222 +1,312 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for osx10.15 (x86_64)
+--
+-- Host: localhost    Database: hotel_de_asiana
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 USE hotel_de_asiana;
--- Insert rows into table 'food'
-INSERT INTO food
-VALUES
-( -- first row: values for the columns
- 1, 'vegetable_rice_and_curry', 'plate_service', 350, 'full'
-),
-( -- second row: values for the columns
- 2, 'chicken_rice_and_curry', 'plate_service', 280, 'half'
-);
+--
+-- Dumping data for table `bill`
+--
 
--- Insert rows into table 'room'
-INSERT INTO room
-VALUES
-( -- first row: values for the columns
- 1, 'single', 'occupied', '1st_floor', 5000
-),
-( -- second row: values for the columns
- 2, 'single', 'vacant', '1st_floor', 5000
-),
-( -- third row: values for the columns
- 101, 'family', 'vacant', '2nd_floor', 12000
-),
-( -- fourth row: values for the columns
- 102, 'family', 'occupied', '2nd_floor', 12000
-);
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'facility'
-INSERT INTO facility
-VALUES
-( -- first row: values for the columns
- 1, 'spa', 'luxury', 2000, '4th_floor'
-),
-( -- second row: values for the columns
- 2, 'public_computer', 'normal', 200, '1st_floor'
-),
-( -- third row: values for the columns
- 3, 'pool', 'luxury', 6000, 'outdoor'
-),
-( -- fourth row: values for the columns
- 4, 'playground', 'normal', 1500, 'outdoor'
-);
+--
+-- Dumping data for table `chef`
+--
 
--- Insert rows into table 'outdoor_guiding_req'
-INSERT INTO outdoor_guiding_req
-VALUES
-( -- first row: values for the columns in the list above
- 4, 'lorem'
-);
+LOCK TABLES `chef` WRITE;
+/*!40000 ALTER TABLE `chef` DISABLE KEYS */;
+INSERT INTO `chef` VALUES (1,1);
+/*!40000 ALTER TABLE `chef` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'outdoor_special_req'
-INSERT INTO outdoor_special_req
-VALUES
-( -- first row: values for the columns in the list above
- 3, 'ipsum'
-);
+--
+-- Dumping data for table `clean`
+--
 
--- Insert rows into table 'indoor_req'
-INSERT INTO indoor_req
-VALUES
-( -- first row: values for the columns in the list above
- 1, 'ipsum'
-),
-( -- second row: values for the columns in the list above
- 2, 'ipsum'
-);
+LOCK TABLES `clean` WRITE;
+/*!40000 ALTER TABLE `clean` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clean` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'guest'
-INSERT INTO guest
-VALUES
-( -- first row: values for the columns
- 1, 'company'
-),
-( -- second row: values for the columns
- 2, 'individual'
-),
-( -- third row: values for the columns
- 3, 'family'
-);
+--
+-- Dumping data for table `cleaning_staff`
+--
 
--- Insert rows into table 'company'
-INSERT INTO company
-VALUES
-( -- first row: values for the columns
- 1, 'Hayleys_PLC', 'No.400 Ven Baddegama Wimalawansa Mawatha, Colombo 01000', 1
-);
+LOCK TABLES `cleaning_staff` WRITE;
+/*!40000 ALTER TABLE `cleaning_staff` DISABLE KEYS */;
+INSERT INTO `cleaning_staff` VALUES (1,'sweeping','1st_floor',3);
+/*!40000 ALTER TABLE `cleaning_staff` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'individual'
-INSERT INTO individual
-VALUES
-( -- first row: values for the columns
- 790029871, 'Saman_Kumara', 'M', 2
-);
+--
+-- Dumping data for table `company`
+--
 
--- Insert rows into table 'family'
-INSERT INTO family
-VALUES
-( -- first row: values for the columns
- 670015893, 'Sumith_Perera', 'M', 3
-);
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+INSERT INTO `company` VALUES (1,'Hayleys','Colombo, SL',1),(2,'Keels ','Colombo, SL',10),(3,'Facebook','California, US',18),(4,'Google','California, US',19),(5,'Amazon','Washington, US',20);
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'contact'
-INSERT INTO contact
-VALUES
-( -- first row: values for the columns
- 0112696331, 1
-),
-( -- second row: values for the columns
- 0773465125, 1
-),
-( -- third row: values for the columns
- 0708954342, 2
-),
-( -- fourth row: values for the columns
- 0714576453, 3
-),
-( -- fifth row: values for the columns
- 0716734235, 3
-);
+--
+-- Dumping data for table `contact`
+--
 
--- Insert rows into table 'employee'
-INSERT INTO employee
-VALUES
-( -- first row: values for the columns
- 1, 'Sarath_Fernando', 'M', 0764538974, '2002-08-20 08:00:00', 'M-1'
-),
-( -- second row: values for the columns
- 2, 'Kumari_Gunawardana', 'F', 0753965937, '2015-06-12 08:00:00', 'M-2'
-),
-( -- third row: values for the columns
- 3, 'Gunapala', 'M', 0713457323, '2010-08-21 08:00:00', 'CL-1'
-),
-( -- fourth row: values for the columns
- 4, 'Pawani_Gunathilaka', 'F', 0713344556, '2018-09-11 08:00:00', 'R-1'
-),
-( -- fifth row: values for the columns
- 5, 'Sumith_Wijewardana', 'M', 0700982823, '2018-10-16 08:00:00', 'CH-1'
-),
-( -- sizth row: values for the columns
- 6, 'Ruwani_Perera', 'F', 0714683517, '2020-06-01 08:00:00', 'KP-1'
-),
-( -- first row: values for the columns
- 7, 'Nuwan_Pradeep', 'M', 0768065116, '2020-07-11 08:00:00', 'W-1'
-);
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` VALUES (112696331,1),(773465125,1),(708954342,2);
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'management'
-INSERT INTO management
-VALUES
-( -- first row: values for the columns
- 1, 'E-1', 'executive', 1
-),
-( -- second row: values for the columns
- 2, 'SS-1', 'assistant', 2
-);
+--
+-- Dumping data for table `employee`
+--
 
--- Insert rows into table 'cleaning_staff'
-INSERT INTO cleaning_staff
-VALUES
-( -- first row: values for the columns
- 1, 'sweeping', '1st_floor', 3
-);
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'Sarath_Fernando','M',764538974,'2002-08-20 08:00:00','M-1'),(2,'Kumari_Gunawardana','F',753965937,'2015-06-12 08:00:00','M-2'),(3,'Gunapala','M',713457323,'2010-08-21 08:00:00','CL-1'),(4,'Pawani_Gunathilaka','F',713344556,'2018-09-11 08:00:00','R-1'),(5,'Sumith_Wijewardana','M',700982823,'2018-10-16 08:00:00','CH-1'),(6,'Ruwani_Perera','F',714683517,'2020-06-01 08:00:00','KP-1'),(7,'Nuwan_Pradeep','M',768065116,'2020-07-11 08:00:00','W-1');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'receptionist'
-INSERT INTO receptionist
-VALUES
-( -- first row: values for the columns
- 1, 4
-);
+--
+-- Dumping data for table `facility`
+--
 
--- Insert rows into table 'kitchen_staff'
-INSERT INTO kitchen_staff
-VALUES
-( -- first row: values for the columns
- 1, 'executive_chef', '7_years', 5
-),
-( -- second row: values for the columns
- 2, 'kitchen_porter', '1_year', 6
-),
-( -- third row: values for the columns
- 3, 'waiter', '2_years', 7
-);
+LOCK TABLES `facility` WRITE;
+/*!40000 ALTER TABLE `facility` DISABLE KEYS */;
+INSERT INTO `facility` VALUES (1,'spa','luxury',2000.00,'4th_floor'),(2,'public_computer','normal',200.00,'1st_floor'),(3,'pool','luxury',6000.00,'outdoor'),(4,'playground','normal',1500.00,'outdoor');
+/*!40000 ALTER TABLE `facility` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'chef'
-INSERT INTO chef
-VALUES
-( -- first row: values for the columns
- 1, 1
-);
+--
+-- Dumping data for table `facility_manage`
+--
 
--- Insert rows into table 'speciality_area'
-INSERT INTO specility_area
-VALUES
-( -- first row: values for the columns
- 'caterer', 1
-),
-( -- second row: values for the columns
- 'personal_chef', 1
-);
+LOCK TABLES `facility_manage` WRITE;
+/*!40000 ALTER TABLE `facility_manage` DISABLE KEYS */;
+INSERT INTO `facility_manage` VALUES (2,1),(3,1),(1,2),(4,2);
+/*!40000 ALTER TABLE `facility_manage` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'support_staff'
-INSERT INTO support_staff
-VALUES
-( -- first row: values for the columns
- 1, 6, 2
-);
+--
+-- Dumping data for table `family`
+--
 
--- Insert rows into table 'waiter'
-INSERT INTO waiter
-VALUES
-( -- first row: values for the columns
- 1, 3
-);
+LOCK TABLES `family` WRITE;
+/*!40000 ALTER TABLE `family` DISABLE KEYS */;
+INSERT INTO `family` VALUES (123468921,'Jon Snow','M',14),(237214622,'Cersei Lannister','F',17),(456798123,'James Hetfield','M',22);
+/*!40000 ALTER TABLE `family` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insert rows into table 'speciality_style'
-INSERT INTO specility_style
-VALUES
-( -- first row: values for the columns
- 1, 'french_service'
-),
-( -- second row: values for the columns
- 1, 'english_service'
-);
+--
+-- Dumping data for table `food`
+--
+
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+INSERT INTO `food` VALUES (1,'Veg Rice','Vegitarian','400','Full Rice'),(3,'Big Boss Beef Burger - B4','American','750','Full Size'),(4,'Club Sandwich','American','350','2 Pieces'),(5,'BBQ Piston Burger','American','700','Half Size'),(6,'Chicken Biryani','Sri Lankan','440','Full Rice'),(7,'Veg Burger Delight ','Vegitarian','570','Full Size');
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `food_order`
+--
+
+LOCK TABLES `food_order` WRITE;
+/*!40000 ALTER TABLE `food_order` DISABLE KEYS */;
+INSERT INTO `food_order` VALUES (2,1,'2020-10-12 00:06:53'),(1,3,'2020-10-12 00:16:44'),(22,4,'2020-10-12 00:22:04'),(19,6,'2020-10-12 00:16:11'),(22,7,'2020-10-12 10:50:09');
+/*!40000 ALTER TABLE `food_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `guest`
+--
+
+LOCK TABLES `guest` WRITE;
+/*!40000 ALTER TABLE `guest` DISABLE KEYS */;
+INSERT INTO `guest` VALUES (1,'company'),(2,'individual'),(10,'company'),(13,'individual'),(14,'family'),(17,'family'),(18,'company'),(19,'company'),(20,'company'),(22,'family');
+/*!40000 ALTER TABLE `guest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `guest_facility`
+--
+
+LOCK TABLES `guest_facility` WRITE;
+/*!40000 ALTER TABLE `guest_facility` DISABLE KEYS */;
+/*!40000 ALTER TABLE `guest_facility` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `guest_room`
+--
+
+LOCK TABLES `guest_room` WRITE;
+/*!40000 ALTER TABLE `guest_room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `guest_room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `handle`
+--
+
+LOCK TABLES `handle` WRITE;
+/*!40000 ALTER TABLE `handle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `handle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `individual`
+--
+
+LOCK TABLES `individual` WRITE;
+/*!40000 ALTER TABLE `individual` DISABLE KEYS */;
+INSERT INTO `individual` VALUES (123467549,'Jon Doe','M',13),(790029871,'Saman Kumara','M',2);
+/*!40000 ALTER TABLE `individual` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `indoor_req`
+--
+
+LOCK TABLES `indoor_req` WRITE;
+/*!40000 ALTER TABLE `indoor_req` DISABLE KEYS */;
+INSERT INTO `indoor_req` VALUES (1,'ipsum'),(2,'ipsum');
+/*!40000 ALTER TABLE `indoor_req` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `kitchen_staff`
+--
+
+LOCK TABLES `kitchen_staff` WRITE;
+/*!40000 ALTER TABLE `kitchen_staff` DISABLE KEYS */;
+INSERT INTO `kitchen_staff` VALUES (1,'executive_chef','7_years',5),(2,'kitchen_porter','1_year',6),(3,'waiter','2_years',7);
+/*!40000 ALTER TABLE `kitchen_staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `management`
+--
+
+LOCK TABLES `management` WRITE;
+/*!40000 ALTER TABLE `management` DISABLE KEYS */;
+INSERT INTO `management` VALUES (1,'E-1','executive',1),(2,'SS-1','assistant',2);
+/*!40000 ALTER TABLE `management` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `outdoor_guiding_req`
+--
+
+LOCK TABLES `outdoor_guiding_req` WRITE;
+/*!40000 ALTER TABLE `outdoor_guiding_req` DISABLE KEYS */;
+INSERT INTO `outdoor_guiding_req` VALUES (4,'lorem');
+/*!40000 ALTER TABLE `outdoor_guiding_req` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `outdoor_special_req`
+--
+
+LOCK TABLES `outdoor_special_req` WRITE;
+/*!40000 ALTER TABLE `outdoor_special_req` DISABLE KEYS */;
+INSERT INTO `outdoor_special_req` VALUES (3,'ipsum');
+/*!40000 ALTER TABLE `outdoor_special_req` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `prepare`
+--
+
+LOCK TABLES `prepare` WRITE;
+/*!40000 ALTER TABLE `prepare` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prepare` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `receptionist`
+--
+
+LOCK TABLES `receptionist` WRITE;
+/*!40000 ALTER TABLE `receptionist` DISABLE KEYS */;
+INSERT INTO `receptionist` VALUES (1,4);
+/*!40000 ALTER TABLE `receptionist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES (1,'Single','Occupied','Ground Floor',5000.00),(2,'Single','Vacant','Ground Floor',5000.00),(102,'Family','Vacant','Second Floor',12000.00),(106,'Single','Occupied','First Floor',7500.00),(107,'Single','Vacant','Second Floor',10000.00),(108,'Family','Occupied','Third Floor',20000.00);
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `specility_area`
+--
+
+LOCK TABLES `specility_area` WRITE;
+/*!40000 ALTER TABLE `specility_area` DISABLE KEYS */;
+INSERT INTO `specility_area` VALUES ('caterer',1),('personal_chef',1);
+/*!40000 ALTER TABLE `specility_area` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `specility_style`
+--
+
+LOCK TABLES `specility_style` WRITE;
+/*!40000 ALTER TABLE `specility_style` DISABLE KEYS */;
+INSERT INTO `specility_style` VALUES (1,'english_service'),(1,'french_service');
+/*!40000 ALTER TABLE `specility_style` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `support_staff`
+--
+
+LOCK TABLES `support_staff` WRITE;
+/*!40000 ALTER TABLE `support_staff` DISABLE KEYS */;
+INSERT INTO `support_staff` VALUES (1,6,2);
+/*!40000 ALTER TABLE `support_staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `waiter`
+--
+
+LOCK TABLES `waiter` WRITE;
+/*!40000 ALTER TABLE `waiter` DISABLE KEYS */;
+INSERT INTO `waiter` VALUES (1,3);
+/*!40000 ALTER TABLE `waiter` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-12 11:05:47
